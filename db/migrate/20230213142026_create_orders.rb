9eba62934469ce1e4035ec92d1a915c94e9115a8
kakisawaa <kakisawaa@gmail.com>
2023-02-15 22:49:59 +0900

    #31 Orders関連のmigrationファイル内のreferencesにnull: false制約を追加

--- db/migrate/20230213142026_create_orders.rb
+++ db/migrate/20230213142026_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.timestamp :order_date
       t.string :order_number, limit: 16
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
