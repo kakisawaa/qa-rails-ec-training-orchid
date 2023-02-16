0ccde0205053255971eed4dcf52c4f396eb003cc
Yuki.tachi <y.tachi0219@gmail.com>
2023-02-17 00:51:49 +0900

    #29 seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@ ActiveRecord::Base.transaction do
   UserClassification.create!(user_classification_name: "general")
   3.times do |i|
     User.create!(
-      password: "qwerty",
+      password: "qwerty123",
       last_name: "user#{i}",
       first_name: "user#{i}",
-      zipcode: "731-319#{i}",
+      zipcode: "731319#{i}",
       prefecture: "広島県",
       municipality: "広島市安佐南区",
       address: "大塚東",
       apartments: "1-1-#{i}",
       email: "a#{i}@example.com",
       phone_number: "082123000#{i}",
-      user_classification_id: 1,
+      user_classification: UserClassification.find_by(user_classification_name: "general"),
       company_name: "株式会社aaa",
     )
   end
