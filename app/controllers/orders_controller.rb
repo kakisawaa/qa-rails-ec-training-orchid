ee01268d6acb146fc8fb35e9897a9078f356d590
kakisawaa <kakisawaa@gmail.com>
2023-03-01 23:37:28 +0900

    #18 注文状態および「注文をキャンセルする」ボタンの表示に関わる条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
     if @order
-      @order_details = OrderDetail.all
+      @order_details = OrderDetail.where(order_id: @order.id)
     end
   end
 end
