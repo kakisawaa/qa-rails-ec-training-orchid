c3009487ce76f12ef05355f581ad0e9f43be0d8f
kakisawaa <kakisawaa@gmail.com>
2023-03-03 00:27:15 +0900

    #18 orders_controller.rbから不要な定義を削除、orderとorder_detailのモデルファイルにメソッドを定義、モデルのメソッドに従ってshow.html.erbの注文状態の条件分岐と小計・合計金額を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,5 @@
 class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
-    if @order
-      @order_details = OrderDetail.where(order_id: @order.id)
-    end
   end
 end
