baf1eb28094097c736d4883bcc149a759ec7b3f2
kakisawaa <kakisawaa@gmail.com>
2023-03-05 18:20:32 +0900

    #13 /users_controller.rbからinclude削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  include SessionsHelper
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:show, :edit, :update]
 
