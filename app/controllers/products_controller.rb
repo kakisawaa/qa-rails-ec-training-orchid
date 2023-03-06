bf382765c0ca0aa3d3ad84128c38e50a5bedfccc
kakisawaa <kakisawaa@gmail.com>
2023-03-06 17:18:37 +0900

    #11 rubocopによる指摘箇所修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
-  def show
-    @product = Product.find_by(id: params[:id])
-  end
-
   def index
     @products = Product.all.page(params[:page]).per(10)
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
   end
 end
